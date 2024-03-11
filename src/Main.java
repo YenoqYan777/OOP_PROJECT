ce49927e3190cc7e593af172e438d5bc273415c5
Yenoq <enoqyanenoq1213@gmail.com>
2024-03-11 22:20:15 +0400

    Default Settings Reset

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+package src;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from the best team!");
-        System.out.println("dasd");
-        System.out.println("ELSE");
     }
 }
