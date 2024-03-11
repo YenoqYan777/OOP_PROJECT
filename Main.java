b1e31a837441d0b5181395505329207d4197e5f9
David Dallakyan <daviddallakyan20051@gmail.com>
2024-03-11 22:09:55 +0400

    ELI YESEM

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from the best team!");
+        System.out.println("dasd");
+        System.out.println("ELSE");
     }
 }
